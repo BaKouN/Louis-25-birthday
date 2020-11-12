3d563dcd489d0b73ac2e4092cf74d27d5804e559
Haroun BAKHOUCHE <hbakhouche@amiltone.com>
2020-11-12 21:52:37 +0100

    camera tracking w/ math

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,40 +1,57 @@
-// width pure center = -935px;
-// height pure center = -495px;
-
 const squareCenter = {
-  height: 25,
-  width: 25,
+  height: $("#square").width() / 2,
+  width: $("#square").width() / 2,
 };
-const containerElt = document.getElementById("container");
 const wToRemove = $(window).width() / 2 - squareCenter.width;
 const hToRemove = $(window).height() / 2 - squareCenter.height;
 
-console.log("width", wToRemove, "height", hToRemove);
-
 $("#move1").click(() => {
   // x: 3330 , y : 1600
-  const xStep1 = 3330;
-  const yStep1 = 1600;
-  gsap.to("#container", { duration: 2, backgroundPosition: "-2550px -1200px" });
+  const xStep1 = 3281;
+  const yStep1 = 1717;
+  gsap.to("#container", {
+    duration: 2,
+    backgroundPosition: `-${xStep1 - wToRemove}px -${yStep1 - hToRemove}px`,
+  });
 });
 
 $("#move2").click(() => {
   // x: 4900 , y : 2400
-  const xStep2 = 3330;
-  const yStep2 = 1600;
-  gsap.to("#container", { duration: 2, backgroundPosition: "-4100px -2000px" });
+  const xStep2 = 4885;
+  const yStep2 = 2321;
+  gsap.to("#container", {
+    duration: 2,
+    backgroundPosition: `-${xStep2 - wToRemove}px -${yStep2 - hToRemove}px`,
+  });
 });
 
 $("#move3").click(() => {
   // x: 965, y: 3700
-  const xStep3 = 3330;
-  const yStep3 = 1600;
-  gsap.to("#container", { duration: 2, backgroundPosition: "0px -3200px" });
+  const xStep3 = 757;
+  const yStep3 = 3707;
+  gsap.to("#container", {
+    duration: 2,
+    backgroundPosition: `-${xStep3 - wToRemove}px -${yStep3 - hToRemove}px`,
+  });
 });
 
 $("#move4").click(() => {
   // x:7100, y: 3900
-  const xStep4 = 3330;
-  const yStep4 = 1600;
-  gsap.to("#container", { duration: 2, backgroundPosition: "-6200px -3400px" });
+  const xStep4 = 6630;
+  const yStep4 = 3425;
+  gsap.to("#container", {
+    duration: 2,
+    backgroundPosition: `-${xStep4 - wToRemove}px -${yStep4 - hToRemove}px`,
+  });
 });
+
+$("#move5").click(() => {
+  const xStep5 = 1939;
+  const yStep5 = 2495;
+  gsap.to("#container", {
+    duration: 2,
+    backgroundPosition: `-${xStep5 - wToRemove}px -${yStep5 - hToRemove}px`,
+  });
+});
+
+// 1769 2417
