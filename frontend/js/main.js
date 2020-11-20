30fb77ff980b5ae6e0e1d0a0101c31e022aa8225
Haroun BAKHOUCHE <hbakhouche@amiltone.com>
2020-11-20 11:21:14 +0100

    changed size of elements + working w/ resize

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,199 +1,127 @@
-const step1EltCenter = {
-  height: $("#step1Elt").width() / 2,
-  width: $("#step1Elt").width() / 2,
-};
-let wToRemove = $(window).width() / 2 - step1EltCenter.width;
-let hToRemove = $(window).height() / 2 - step1EltCenter.height;
-
-$(window).resize(() => {
-  wToRemove = $(window).width() / 2 - step1EltCenter.width;
-  hToRemove = $(window).height() / 2 - step1EltCenter.height;
-});
-
 const accueil = {
-  elt: "#accueil",
+  btnElt: '#start',
+  containerElt: '#accueil',
   xPos: 2380,
   yPos: 4215,
 };
 
 const step1 = {
-  elt: "#step1Elt",
-  xPos: 4710,
-  yPos: 4170,
+  btnElt: '#step1',
+  containerElt: '#step1Elt',
+  xPos: 4712,
+  yPos: 4157,
 };
 
 const step2 = {
-  elt: "#step2Elt",
-  xPos: 4400,
-  yPos: 1950,
+  btnElt: '#step2',
+  containerElt: '#step2Elt',
+  xPos: 4386,
+  yPos: 1948,
 };
 
 const step3 = {
-  elt: "#step3Elt",
-  xPos: 2845,
-  yPos: 1969,
+  btnElt: '#step3',
+  containerElt: '#step3Elt',
+  xPos: 2868,
+  yPos: 1944,
 };
 
 const step4 = {
-  elt: "#step4Elt",
-  xPos: 6289,
-  yPos: 3125,
+  btnElt: '#step4',
+  containerElt: '#step4Elt',
+  xPos: 6304,
+  yPos: 3104,
 };
 
 const step5 = {
-  elt: "#step5Elt",
-  xPos: 7709,
+  btnElt: '#step5',
+  containerElt: '#step5Elt',
+  xPos: 7724,
   yPos: 2061,
 };
 
 const step6 = {
-  elt: "#step6Elt",
-  xPos: 8189,
-  yPos: 4249,
+  btnElt: '#step6',
+  containerElt: '#step6Elt',
+  xPos: 8206,
+  yPos: 4224,
 };
 
-const centerAnimation = {
-  duration: 2,
-  x: `${wToRemove}px`,
-  y: `${hToRemove}px`,
-};
+const animationDuration = 2;
+let wToRemove = $(window).width() / 2;
+let hToRemove = $(window).height() / 2;
+let stateMachine = accueil;
 
-function moveElements(source, objects) {
-  gsap.to(source.elt, centerAnimation);
-  objects.forEach((object) => {
-    gsap.to(object.elt, {
-      duration: 2,
-      x: `${wToRemove + object.xPos - source.xPos}px`,
-      y: `${hToRemove + object.yPos - source.yPos}px`,
-    });
+const eltsArray = [accueil, step1, step2, step3, step4, step5, step6];
+
+// call with sourceObj and an array containing all the other objects
+function moveElements(source, duration) {
+  // move background
+  gsap.to('#container', {
+    duration: duration,
+    backgroundPosition: `
+    -${source.xPos - wToRemove}px 
+    -${source.yPos - hToRemove}px
+    `,
   });
+  // get sourceElt in the center of the screen
+  gsap.to(source.containerElt, {
+    duration: duration,
+    x: `${wToRemove}px`,
+    y: `${hToRemove}px`,
+  });
+  // move all other elements in sync relatively to the source one
+  eltsArray
+    .filter((elt) => elt !== source)
+    .forEach((object) => {
+      gsap.to(object.containerElt, {
+        duration: duration,
+        x: `${wToRemove + object.xPos - source.xPos}px`,
+        y: `${hToRemove + object.yPos - source.yPos}px`,
+      });
+    });
 }
 
-// ! With BackgroundPosition : minus (-) ToRemove ! With x & y : plus (+) ToRemove
 // set starting position to start
-gsap.set("#container", {
-  backgroundPosition: `-${accueil.xPos - wToRemove}px -${
-    accueil.yPos - hToRemove
-  }px`,
-});
-gsap.set("#accueil", {
-  x: `${wToRemove}px`,
-  y: `${hToRemove}px`,
-});
-gsap.set("#step1Elt", {
-  x: `${wToRemove + (step1.xPos - accueil.xPos)}px`,
-  y: `${hToRemove + (step1.yPos - accueil.yPos)}px`,
-});
-gsap.set("#step2Elt", {
-  x: `${wToRemove + (step2.xPos - accueil.xPos)}px`,
-  y: `${hToRemove + (step2.yPos - accueil.yPos)}px`,
+moveElements(accueil, 0);
+
+$(accueil.btnElt).click(() => {
+  moveElements(accueil, animationDuration);
+  stateMachine = accueil;
 });
 
-$("#start").click(() => {
-  // Move bgPosition to the center of new element
-  gsap.to("#container", {
-    duration: 2,
-    backgroundPosition: `-${accueil.xPos - wToRemove}px -${
-      accueil.yPos - hToRemove
-    }px`,
-  });
-  gsap.to("#accueil", {
-    duration: 2,
-    x: `${wToRemove}px`,
-    y: `${hToRemove}px`,
-  });
-  gsap.to("#step1Elt", {
-    duration: 2,
-    x: `${wToRemove + step1.xPos - accueil.xPos}px`,
-    y: `${hToRemove + step1.yPos - accueil.yPos}px`,
-  });
-  gsap.to("#step2Elt", {
-    duration: 2,
-    x: `${wToRemove + step2.xPos - accueil.xPos}px`,
-    y: `${hToRemove + step2.yPos - accueil.yPos}px`,
-  });
+$(step1.btnElt).click(() => {
+  moveElements(step1, animationDuration);
+  stateMachine = step1;
 });
 
-$("#step1").click(() => {
-  gsap.to("#container", {
-    duration: 2,
-    backgroundPosition: `-${step1.xPos - wToRemove}px -${
-      step1.yPos - hToRemove
-    }px`,
-  });
-  gsap.to("#step1Elt", {
-    duration: 2,
-    x: `${wToRemove}px`,
-    y: `${hToRemove}px`,
-  });
-  gsap.to("#accueil", {
-    duration: 2,
-    x: `${wToRemove + accueil.xPos - step1.xPos}px`,
-    y: `${hToRemove + accueil.yPos - step1.yPos}px`,
-  });
-  gsap.to("#step2Elt", {
-    duration: 2,
-    x: `${wToRemove + step2.xPos - step1.xPos}px`,
-    y: `${hToRemove + step2.yPos - step1.yPos}px`,
-  });
+$(step2.btnElt).click(() => {
+  moveElements(step2, animationDuration);
+  stateMachine = step2;
 });
 
-$("#step2").click(() => {
-  gsap.to("#container", {
-    duration: 2,
-    backgroundPosition: `-${step2.xPos - wToRemove}px -${
-      step2.yPos - hToRemove
-    }px`,
-  });
-  gsap.to("#step2Elt", {
-    duration: 2,
-    x: `${wToRemove}px`,
-    y: `${hToRemove}px`,
-  });
-  gsap.to("#step1Elt", {
-    duration: 2,
-    x: `${wToRemove + step1.xPos - step2.xPos}px`,
-    y: `${hToRemove + step1.yPos - step2.yPos}px`,
-  });
-  gsap.to("#accueil", {
-    duration: 2,
-    x: `${wToRemove + accueil.xPos - step2.xPos}px`,
-    y: `${hToRemove + accueil.yPos - step2.yPos}px`,
-  });
+$(step3.btnElt).click(() => {
+  moveElements(step3, animationDuration);
+  stateMachine = step3;
 });
 
-$("#step3").click(() => {
-  gsap.to("#container", {
-    duration: 2,
-    backgroundPosition: `-${step3.xPos - wToRemove}px -${
-      step3.yPos - hToRemove
-    }px`,
-  });
+$(step4.btnElt).click(() => {
+  moveElements(step4, animationDuration);
+  stateMachine = step4;
 });
 
-$("#step4").click(() => {
-  gsap.to("#container", {
-    duration: 2,
-    backgroundPosition: `-${step4.xPos - wToRemove}px -${
-      step4.yPos - hToRemove
-    }px`,
-  });
+$(step5.btnElt).click(() => {
+  moveElements(step5, animationDuration);
+  stateMachine = step5;
 });
 
-$("#step5").click(() => {
-  gsap.to("#container", {
-    duration: 2,
-    backgroundPosition: `-${step5.xPos - wToRemove}px -${
-      step5.yPos - hToRemove
-    }px`,
-  });
+$(step6.btnElt).click(() => {
+  moveElements(step6, animationDuration);
+  stateMachine = step6;
 });
 
-$("#step6").click(() => {
-  gsap.to("#container", {
-    duration: 2,
-    backgroundPosition: `-${step6.xPos - wToRemove}px -${
-      step6.yPos - hToRemove
-    }px`,
-  });
+$(window).resize(() => {
+  wToRemove = $(window).width() / 2;
+  hToRemove = $(window).height() / 2;
+  moveElements(stateMachine, 0);
 });
